3fd3f92ea555c1b044a6da1ee11fe9a49c91054f
uvsq21805839 <dskprod@gmail.com>
2020-05-02 04:40:49 +0200

    first

---
+++ src/main/java/pglp_9/pglp_9/App.java
@@ -0,0 +1,13 @@
+package pglp_9.pglp_9;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
